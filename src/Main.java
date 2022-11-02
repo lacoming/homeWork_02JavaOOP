6a2751e0abae72dd10859e21e92a61c6dc17d3ca
lacoming <65919460+lacoming@users.noreply.github.com>
2022-11-02 17:31:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите фамилию: ");
+        String lastName = input.nextLine();
+
+        System.out.println("Введите имя: ");
+        String firstName = input.nextLine();
+
+        System.out.println("Введите кем вы работаете: ");
+        String profession = input.nextLine();
+
+        System.out.println(lastName + " " + firstName);
+        System.out.println("Профессия: " + profession);
+    }
+}
