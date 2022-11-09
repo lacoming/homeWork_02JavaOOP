1853cd8bc6c88fc843e26fb622c9cfbf7588ff3f
lacoming <65919460+lacoming@users.noreply.github.com>
2022-11-09 13:44:04 +0300

    Change package

---
+++ src/Additional_Tasks_Cycle_2/Main.java
@@ -0,0 +1,19 @@
+package Additional_Tasks_Cycle_2;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите любое слово или предложение, а я разобъю этот текст посимволльно: ");
+
+        String text = input.nextLine();
+
+        char [] symbols = text.toCharArray();
+
+        for (char symbol : symbols) {
+            System.out.println(symbol);
+        }
+    }
+}
