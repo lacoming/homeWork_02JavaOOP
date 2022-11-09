325d084e5fbf9eb9e3ace420165ccba92f2044dd
lacoming <65919460+lacoming@users.noreply.github.com>
2022-11-09 13:35:14 +0300

    Change package

---
+++ src/Additional_Tasks_2/Main.java
@@ -0,0 +1,33 @@
+package Additional_Tasks_2;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int side1;
+        int side2;
+        int side3;
+
+        System.out.println("Введите длину первой стороны треугольника: ");
+
+        side1 = input.nextInt();
+
+        System.out.println("Введите длину второй стороны треугольника: ");
+
+        side2 = input.nextInt();
+
+        System.out.println("Введите длину третьей стороны треугольника: ");
+
+        side3 = input.nextInt();
+
+        if (side1 == side2 && side1 == side3)
+            System.out.println("Это равносторонний треугольник");
+        else {
+            if (side1 == side2 || side1 == side3 || side2 == side3)
+                System.out.println("Это равнобедренный треугольник");
+            else
+                System.out.println("Это разносторонний треугольник");
+        }
+    }
+}
